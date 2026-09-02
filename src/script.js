bb123b24d0afee7f461c90bad5862ededced5714
Ashish Shinde <sunnys.shinde22@gmail.com>
2026-09-02 19:47:07 +0530

    Added ghosts

--- src/script.js
+++ src/script.js
@@ -280,6 +280,12 @@ const doorLight = new THREE.PointLight("#ff7d46", 10)
 doorLight.position.set(0, 1.1, 2.2)
 house.add(doorLight)
 
+// Ghosts
+const ghost1 = new THREE.PointLight('#8800ff', 6)
+const ghost2 = new THREE.PointLight('#ff0088', 6)
+const ghost3 = new THREE.PointLight('#ff0000', 6)
+scene.add(ghost1, ghost2, ghost3)
+
 /**
  * Sizes
  */
@@ -336,6 +342,22 @@ const tick = () =>
     // Timer
     timer.update()
     const elapsedTime = timer.getElapsed()
+
+    // Ghost
+    const ghost1Angle = elapsedTime * 0.5
+    ghost1.position.x = Math.sin(ghost1Angle) * 4
+    ghost1.position.z = Math.cos(ghost1Angle) * 4
+    ghost1.position.y = Math.sin(ghost1Angle) * Math.sin(ghost1Angle * 2.34) * Math.sin(ghost1Angle * 3.45)
+
+    const ghost2Angle = - elapsedTime * 0.38
+    ghost2.position.x = Math.sin(ghost2Angle) * 5
+    ghost2.position.z = Math.cos(ghost2Angle) * 5
+    ghost2.position.y = Math.sin(ghost2Angle) * Math.sin(ghost2Angle * 2.34) * Math.sin(ghost2Angle * 3.45)
+
+    const ghost3Angle = elapsedTime * 0.23
+    ghost3.position.x = Math.sin(ghost3Angle) * 6
+    ghost3.position.z = Math.cos(ghost3Angle) * 6
+    ghost3.position.y = Math.sin(ghost3Angle) * Math.sin(ghost3Angle * 2.34) * Math.sin(ghost3Angle * 3.45)
 
     // Update controls
     controls.update()
