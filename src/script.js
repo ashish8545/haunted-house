1b8ee827b9c57ff2543e7670a95082e50b51b36b
Ashish Shinde <sunnys.shinde22@gmail.com>
2026-09-02 20:16:45 +0530

    Added Fog

--- src/script.js
+++ src/script.js
@@ -380,6 +380,12 @@ sky.material.uniforms['mieDirectionalG'].value = 0.95
 sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95)
 
 /**
+ * Fog
+ */
+// scene.fog = new THREE.Fog('#fefefe', 10, 13)
+scene.fog = new THREE.FogExp2('#04343f', 0.1)
+
+/**
  * Animate
  */
 const timer = new Timer()
